73fd7217efc6045d27a1034a5445f5201a5e399e
Bryan Hughes <bryanl.hughes@gmail.com>
2014-04-03 03:26:43 +0000

    Added rewrite basennand also trying to move rewrites to directory

--- site/profiles/manifests/herbalscents.pp
+++ site/profiles/manifests/herbalscents.pp
@@ -4,15 +4,17 @@ class profiles::herbalscents {
 
 
   apache::vhost { 'herbal-scents.com':
-    port     => '80',
-    docroot  => '/var/www/herbal-scents',
-    rewrites => [ { rewrite_rule          => [ '^index\.php$ - [L]',
-                                      '. /index.php [L]' ]
-                  },
-                  { rewrite_cond => [ '%{REQUEST_FILENAME} !-f',
-                                      '%{REQUEST_FILENAME} !-d' ]
-                  }
-    ],
+    port      => '80',
+    docroot   => '/var/www/herbal-scents',
+    directory => { path     => '/var/www/herbal-scents',
+                   rewrites => [ { rewrite_base => '/'
+                                 { rewrite_rule => [ '^index\.php$ - [L]',
+                                                     '. /index.php [L]' ]
+                                 },
+                                 { rewrite_cond => [ '%{REQUEST_FILENAME} !-f',
+                                                     '%{REQUEST_FILENAME} !-d' ]
+                                 }
+                    ],
   }
 
   vcsrepo { '/var/www/herbal-scents':
